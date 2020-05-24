4438a25b655de4f0de5d12c0f560087c9785c7cb
Igor Berezovchuk <chev5@ukr.net>
2020-05-24 15:41:11 +0300

    Updates

---
+++ precache-manifest.15034b9cf3a072447e3c85fdb2786b57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da3b617885789e5b16e367c80a65551",
+    "url": "/marathons-frontend/index.html"
+  },
+  {
+    "revision": "0b190f97e68a511ab2f2",
+    "url": "/marathons-frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2efc88f42ec9437a7771",
+    "url": "/marathons-frontend/static/js/2.e125daba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/marathons-frontend/static/js/2.e125daba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b190f97e68a511ab2f2",
+    "url": "/marathons-frontend/static/js/main.0ebe2411.chunk.js"
+  },
+  {
+    "revision": "83998bb20ffbed674ef4",
+    "url": "/marathons-frontend/static/js/runtime-main.f7cc397b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marathons-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
